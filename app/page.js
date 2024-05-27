4f936d179069d83724aec43413366a7443679469
SimjotKaur <141382281+SimjotKaur@users.noreply.github.com>
2024-05-27 10:34:46 -0600

    First commit

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/https://cprg306-assignments.vercel.app/week-2">
+      <Link href="/week-2">
         Go to Week 2 Assignment
       </Link>
     </main>
