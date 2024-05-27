59edea20443c8d44453045337b6113b2814ff063
SimjotKaur <141382281+SimjotKaur@users.noreply.github.com>
2024-05-27 09:55:36 -0600

    First commit

---
+++ app/week-2/page.js
@@ -0,0 +1,22 @@
+import StudentInfo from "./student-info";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo />
+    </main>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+  
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Simjot Kaur</p>
+      <Link href="https://github.com/SimjotKaur/cprg306-assignments">
+        github
+      </Link>
+    </div>
+  );
+}
